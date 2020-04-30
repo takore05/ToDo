197c444b31219411fd2c2f47a1a3597adf24314f
takore05 <60117058+takore05@users.noreply.github.com>
2020-04-30 19:10:42 -0400

    Updating main.py code
    fixing errors

--- main.py
+++ main.py
@@ -1,20 +1,54 @@
 from tkinter import *
+from tkinter import Checkbutton
+
 import googletrans
 from googletrans import Translator
 
-root = Tk()
 translator = Translator()
+root = Tk()
+to_do = {}
+
+def exitAdding(entrybox,first, second):
+    entrybox.destroy()
+    first.destroy()
+    second.destroy()
+
+def completed(event, check):
+    check.destroy()
+
+def translating(thing):
+    translated = translator.translate(text=thing, dest='fr', src='en').text
+    return translated
 
 
-def translatingTODO(newitem):
-    translated_item = translator.translate(newitem, dest='fr', src='en').text
-    print(translated_item)
-    # add to dictionary with the translated version
+def adding(word):
+    english = word.get()
+    french = translating(english)
+    to_do[english] = french
+    check: Checkbutton = Checkbutton(root, text=french)
+    check.pack()
+
+def buttonPressed():
+    word = StringVar
+    newitem = Entry(root, textvariable = word)
+    newitem.pack()
+    button_2 = Button(root, text = "Add to list?",command = lambda :adding(newitem))
+    button_2.pack()
+    button_3 = Button(root, text = "Stop Adding?", command = lambda: exitAdding(newitem, button_2,button_3))
+    button_3.pack()
 
 
-word = StringVar()
-todothing = Entry(root, textvariable=word)
-todothing.pack()
-translatingTODO(word)
+
+button_1 = Button(root, text="New To Do",command = buttonPressed)
+button_1.pack()
+
+
+
+
+
+
+
+
+
 
 root.mainloop()
